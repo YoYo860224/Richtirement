c458983531a8a6d9c48030bd134bc57b87f7dba5
YoCheng <xyox860224@hotmail.com>
2019-01-10 11:03:52 +0800

    Fix sotryResult by vertical layout.

--- Assets/Scripts/StoryReview/StoryReview.cs
+++ Assets/Scripts/StoryReview/StoryReview.cs
@@ -12,11 +12,11 @@ public class StoryReview : MonoBehaviour {
     private List<string> choice = new List<string>();
     private void Awake()
     {
-        //StoryManager.log.Add(new EventLog(0, new List<bool>() { true, true }));
-        //StoryManager.log.Add(new EventLog(1, new List<bool>() { false, true }));
-        //StoryManager.log.Add(new EventLog(2, new List<bool>() { false }));
-        //StoryManager.log.Add(new EventLog(3, new List<bool>() { true, true }));
-        //StoryManager.log.Add(new EventLog(4, new List<bool>() { true }));
+        StoryManager.log.Add(new EventLog(0, new List<bool>() { true, true }));
+        StoryManager.log.Add(new EventLog(1, new List<bool>() { false, true }));
+        StoryManager.log.Add(new EventLog(2, new List<bool>() { false }));
+        StoryManager.log.Add(new EventLog(3, new List<bool>() { true, true }));
+        StoryManager.log.Add(new EventLog(4, new List<bool>() { true }));
 
         int eventAmount = 0;
         for (int i = 0; i < StoryManager.log.Count; i++)
@@ -25,7 +25,6 @@ public class StoryReview : MonoBehaviour {
             eventAmount += StoryManager.log[i].choice.Count;
 
             Question tempQuestion = StoryManager.AllSEventList[StoryManager.log[i].id].question;
-
             for (int j = 0; j < StoryManager.log[i].choice.Count; j++)
             {
                 question.Add(tempQuestion.content);
@@ -54,16 +53,19 @@ public class StoryReview : MonoBehaviour {
 
             }
         }
+        // 使用 vertical layout 修正看看
+
         RectTransform rt = content.GetComponent<RectTransform>();
         rt.sizeDelta = new Vector2(rt.sizeDelta.x, 520 * eventAmount + 250);
-        content.transform.localPosition = new Vector2(0, 0);
+        // content.transform.localPosition = new Vector2(0, 0);
         for (int i = 0; i < eventAmount; i++)
         {
             GameObject instance = Instantiate(StoryBoxPrefab) as GameObject;
             instance.transform.SetParent(content.transform);
             instance.GetComponent<StoryBox>().SetText(question[i], choice[i]);
-            instance.transform.localPosition = new Vector2(0, i * (-520));
+            // instance.transform.localPosition = new Vector2(0, i * (-520));
         }
+        content.transform.Find("Button").transform.SetSiblingIndex(eventAmount + 1);
     }
 
     // Use this for initialization
