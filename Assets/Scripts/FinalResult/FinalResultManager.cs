0a24844f24ead2a688c0e828fd61e69a91004556
YoCheng <xyox860224@hotmail.com>
2019-01-10 12:35:41 +0800

    Result to Eng.

--- Assets/Scripts/FinalResult/FinalResultManager.cs
+++ Assets/Scripts/FinalResult/FinalResultManager.cs
@@ -150,7 +150,7 @@ public class FinalResultManager : MonoBehaviour {
             && Setting.CharacterSetting.Money > Setting.CharacterSetting.Hearth)
             {
                 storyImage.sprite = Resources.Load<Sprite>("Story/ColorfulLife");
-                resultText.text = "恭喜你！\n不僅金融管理能力佳，還能兼顧健康與心靈平衡、維持良好社交生活，擁有著人人稱羨、豐餘富足、多采多姿的退休生活！\n\n請繼續保持下去，開創嶄新的退休人生扉頁。";
+                resultText.text = "All good among physical,  mental, and social status! Also, you did a good financial management, it's certainly a colorful life!";
             }
             else if (Setting.CharacterSetting.Mental > 0
                 && Setting.CharacterSetting.Mental > Setting.CharacterSetting.Money
@@ -158,7 +158,7 @@ public class FinalResultManager : MonoBehaviour {
                 && Setting.CharacterSetting.Mental > Setting.CharacterSetting.Hearth)
             {
                 storyImage.sprite = Resources.Load<Sprite>("Story/VigorousLife");
-                resultText.text = "或許累積資產不是那麼的多，但懂得如何維持健康心理狀態。\n\n若與人有不錯的社交交流，提升健康層面，這也不失為另一種富足的退休生活。";
+                resultText.text = "The state of mind is excellent, life doesn't have to be too rich, but the most important thing is to keep a good mood, right?";
             }
             else if (Setting.CharacterSetting.Hearth > 0
                 && Setting.CharacterSetting.Hearth > Setting.CharacterSetting.Money
@@ -166,7 +166,7 @@ public class FinalResultManager : MonoBehaviour {
                 && Setting.CharacterSetting.Hearth > Setting.CharacterSetting.Mental)
             {
                 storyImage.sprite = Resources.Load<Sprite>("Story/HappyLife");
-                resultText.text = "健康是良好退休生活的根本原則，掌握得相當好，擁有良好生理狀態！\n\n也別忘要多尋求各領域專業資源，累積更平穩資產收入，同時積極參與社交活動也是維持心理健康的秘方哦！";
+                resultText.text = "Good health status! You're right, life must be healthy to do more things to do!";
             }
             else if (Setting.CharacterSetting.Social > 0
                 && Setting.CharacterSetting.Social > Setting.CharacterSetting.Money
@@ -174,12 +174,12 @@ public class FinalResultManager : MonoBehaviour {
                 && Setting.CharacterSetting.Social > Setting.CharacterSetting.Mental)
             {
                 storyImage.sprite = Resources.Load<Sprite>("Story/BalenceLife");
-                resultText.text = "積極參與社交是一項無形的資產，相信已擁有許多交心朋友、職場貴人，同時也與最重要的親人有緊密的良好關係。\n\n且試著從社交人脈中去找尋專家，請益維持健康、累積資產、提升心理愉悅的方法，讓退休生活達到更佳的平衡。";
+                resultText.text = "Have a good social status, encounter many friends in the life, benefactors in the workplace, and even have a good relationship with the most important relatives.";
             }
             else
             {
                 storyImage.sprite = Resources.Load<Sprite>("Story/NothingButMoney");
-                resultText.text = "擁有足夠維持生活的良好條件與經濟狀態，卻因沒有良好運用導致過著沒有品質的生活，心理層面也未因資產而獲得滿足。\n\n建議嘗試更好的資產運用方式，讓生理、心理、社交層面為良好均衡狀態，提升退休生活的品質。";
+                resultText.text = "Even if you have enough living and finance conditions, but there is no good use of them and you lived a unquality life. What is the meaning of living for you?";
             }
         }
     }
